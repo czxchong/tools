d22992b6fdc2682eca25951cac8be55eb76a5b58
xianchaohong <xianch@mama.cn>
2018-10-27 11:24:05 +0800

    转化maven项目

---
+++ src/main/java/com/xchong/app/demo.java
@@ -0,0 +1,13 @@
+package com.xchong.app;
+
+/**
+ * @author xchong
+ * @version 创建时间：2018/10/27 上午 11:08
+ * （类说明）
+ */
+public class demo {
+
+    public void demo(Object obj) {
+       System.out.println("heleo world");
+    }
+}
